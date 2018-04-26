f5eac59fd84c816cb820e4ab79a7aa83d80a167c
Seth <s3543202@student.rmit.edu.au>
2018-04-26 19:18:46 +1000

    initialise User node with 'coin' sub-node

--- src/authentication/SignUp.js
+++ src/authentication/SignUp.js
@@ -48,6 +48,17 @@ export default class SignUp extends Component {
                      admin: this.state.admin,
                      trading: false
                });
+               firebase.database().ref('user/' + uid + '/coin').set({
+                   BTC: 0,
+                   EOS: 0,
+                   ETH: 0,
+                   LTC: 0,
+                   NEO: 0,
+                   NULS: 0,
+                   XMR: 0,
+                   XRP: 0,
+                   balance: 1000000
+               });
 
            }
        });
