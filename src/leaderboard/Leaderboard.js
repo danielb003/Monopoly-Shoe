6d6f72ad3b8884518bf75fadebd5df7cc098ea5f
Seth <s3543202@student.rmit.edu.au>
2018-05-01 22:52:56 +1000

    Loop history data based on specified Timeframe, then save to State

--- src/leaderboard/Leaderboard.js
+++ src/leaderboard/Leaderboard.js
@@ -6,7 +6,7 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import Typography from 'material-ui/Typography';
-
+import firebase from "firebase";
 
 function TabContainer(props) {
     return (
@@ -28,12 +28,159 @@ class Leaderboard extends Component {
         super(props);
         this.state = {
             tabValue : 0,
+            history: [],
         }
+        this.handleTabChange = this.handleTabChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadHistoryData_andSaveToState();
     }
 
     handleTabChange = (event, tabValue) => {
-        this.setState({ tabValue });
+        this.setState({ tabValue }, () => {
+            this.loadHistoryData_andSaveToState();
+        });
+        alert(tabValue);
+
     };
+
+    loadHistoryData_andSaveToState = () => {
+        const history = firebase.database().ref('history');
+        const filtered_history = []
+        history.on('value', (snapshot) => {
+            if(snapshot.val() != null){
+                var moment = require('moment');
+                var currentTime = moment();
+                // Past 24 Hrs
+                if(this.state.tabValue == 0){
+                    for(const index in snapshot.val()){
+                        const timestamp = snapshot.child(index +"/timestamp").val();
+                        const parseTimestramp = moment(timestamp);
+                        console.log('currentTime :' + currentTime.format());
+                        console.log('parseTimestramp :' + parseTimestramp.format());
+
+                        const hoursDiff = currentTime.diff(parseTimestramp, 'hours');
+                        console.log('diff ' + hoursDiff);
+                        if (hoursDiff <= 24){
+                            filtered_history.push({
+                                user_id: snapshot.child(index + "/user_id").val(),
+                                type: snapshot.child(index + "/type").val(),
+                                amount: snapshot.child(index + "/amount").val(),
+                                price: snapshot.child(index + "/price").val(),
+                                total: snapshot.child(index + "/total").val(),
+                                coinType: snapshot.child(index + "/coinType").val(),
+                                coinValue: snapshot.child(index + "/coinValue").val(),
+                                coinTotal: snapshot.child(index + "/coinTotal").val(),
+                                timestamp: snapshot.child(index + "/timestamp").val()
+                            })
+                        }
+                    }
+                    this.setState({
+                        history: filtered_history
+                    }, () => {
+                        console.log(filtered_history)
+                    });
+                }
+                // Past 1 Week
+                else if(this.state.tabValue == 1){
+                    for(const index in snapshot.val()){
+                        const timestamp = snapshot.child(index +"/timestamp").val();
+                        const parseTimestramp = moment(timestamp);
+                        console.log('currentTime :' + currentTime.format());
+                        console.log('parseTimestramp :' + parseTimestramp.format());
+
+                        const weeksDiff = currentTime.diff(parseTimestramp, 'weeks');
+                        console.log('weeksDiff ' + weeksDiff);
+                        if (weeksDiff < 1){
+                            filtered_history.push({
+                                user_id: snapshot.child(index + "/user_id").val(),
+                                type: snapshot.child(index + "/type").val(),
+                                amount: snapshot.child(index + "/amount").val(),
+                                price: snapshot.child(index + "/price").val(),
+                                total: snapshot.child(index + "/total").val(),
+                                coinType: snapshot.child(index + "/coinType").val(),
+                                coinValue: snapshot.child(index + "/coinValue").val(),
+                                coinTotal: snapshot.child(index + "/coinTotal").val(),
+                                timestamp: snapshot.child(index + "/timestamp").val()
+                            })
+                        }
+                    }
+                    this.setState({
+                        history: filtered_history
+                    }, () => {
+                        console.log(filtered_history)
+                    });
+                }
+                // Past 1 Month
+                else if(this.state.tabValue == 2){
+                    for(const index in snapshot.val()){
+                        const timestamp = snapshot.child(index +"/timestamp").val();
+                        const parseTimestramp = moment(timestamp);
+                        console.log('currentTime :' + currentTime.format());
+                        console.log('parseTimestramp :' + parseTimestramp.format());
+
+                        const monthsDiff = currentTime.diff(parseTimestramp, 'months');
+                        console.log('monthsDiff ' + monthsDiff);
+                        if (monthsDiff < 1){
+                            filtered_history.push({
+                                user_id: snapshot.child(index + "/user_id").val(),
+                                type: snapshot.child(index + "/type").val(),
+                                amount: snapshot.child(index + "/amount").val(),
+                                price: snapshot.child(index + "/price").val(),
+                                total: snapshot.child(index + "/total").val(),
+                                coinType: snapshot.child(index + "/coinType").val(),
+                                coinValue: snapshot.child(index + "/coinValue").val(),
+                                coinTotal: snapshot.child(index + "/coinTotal").val(),
+                                timestamp: snapshot.child(index + "/timestamp").val()
+                            })
+                        }
+                    }
+                    this.setState({
+                        history: filtered_history
+                    }, () => {
+                        console.log(filtered_history)
+                    });
+                }
+                // All Times
+                else if(this.state.tabValue == 3){
+                    for(const index in snapshot.val()){
+                        filtered_history.push({
+                            user_id: snapshot.child(index + "/user_id").val(),
+                            type: snapshot.child(index + "/type").val(),
+                            amount: snapshot.child(index + "/amount").val(),
+                            price: snapshot.child(index + "/price").val(),
+                            total: snapshot.child(index + "/total").val(),
+                            coinType: snapshot.child(index + "/coinType").val(),
+                            coinValue: snapshot.child(index + "/coinValue").val(),
+                            coinTotal: snapshot.child(index + "/coinTotal").val(),
+                            timestamp: snapshot.child(index + "/timestamp").val()
+                        })
+                    }
+                    this.setState({
+                        history: filtered_history
+                    }, () => {
+                        console.log(filtered_history)
+                    });
+                }
+
+            }
+        });
+    }
+
+    // Loop history data based on specified Timeframe
+    // Then save to state
+
+
+    // loop history [] data from state and get user_id
+    // get current crypto value
+    // calculate profit/loss for each history node
+    // sum up profit/loss and calculate in %
+    // assign this profit field to user state
+
+
+    // Loop the user data from state
+    // Then display, ordered by profit and assign Rank
 
     render(){
         const { classes } = this.props;
