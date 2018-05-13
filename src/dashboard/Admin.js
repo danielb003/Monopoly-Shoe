8f4da28e5b9a47318dfbcc1af87ceb8dd884f9c3
danielb003 <bellino-pak@hotmail.com>
2018-05-13 23:40:28 +1000

    Merge branch 'Daniel' of https://github.com/danielb003/Monopoly-Shoe into Daniel
    # Conflicts:
    #	src/dashboard/Dashboard.css

---
+++ src/dashboard/Admin.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Admin.css';
+
+export default class Admin extends Component {
+   constructor(props) {
+      super(props);
+
+   }
+
+   render() {
+      return (
+         <div>
+            <p>Admin page</p>
+         </div>
+      )
+   }
+
+}
