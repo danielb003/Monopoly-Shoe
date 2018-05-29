d3eb1e8c325cfff6d25921f285467ea2c215abed
Seth <s3543202@student.rmit.edu.au>
2018-05-29 20:16:29 +1000

    Add another small test

--- test/Leaderboard.test.js
+++ test/Leaderboard.test.js
@@ -5,39 +5,20 @@ import Adapter from 'enzyme-adapter-react-16';
 // React 16 Enzyme Adapter
 configure({adapter: new Adapter()});
 
-
-import { Board } from "../src/leaderboard/Leaderboard";
+import Leaderboard from "../src/leaderboard/Leaderboard";
 // import { expect } from 'chai';
 
 // import jsdom from 'jsdom'
 // const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
 // global.document = doc
 // global.window = doc.defaultView
-
-// test('render a label', () => {
-//     const wrapper = shallow(
-//         <Board/>
-//     );
-//     expect(wrapper).toMatchSnapshot();
-// });
-
 describe('Component: Leaderboard', () =>{
+    it('tests to be defined', () => {
+        expect(Leaderboard).toBeDefined();
+    })
     it('tests something on leaderboard', () => {
-        let wrapper;
-
-        // beforeEach(() => { wrapper = shallow(<Board/>);});
-
-        beforeEach(() => {
-            wrapper = shallow(<Board/>);
-        });
+        let wrapper = shallow(<Leaderboard/>);
         expect(wrapper).toMatchSnapshot();
-        // expect(wrapper.find({ sellTotal: null })).to.have.length(0);
-        // const wrapper = shallow((
-        //     <div>
-        //         <div data-foo="foo" data-bar="bar">Hello</div>
-        //     </div>
-        // ));
-        // expect(wrapper.contains(<div data-foo="foo" data-bar="bar">Hello</div>)).to.equal(true);
     })
 })
 
